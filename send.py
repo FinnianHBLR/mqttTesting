10932f3eeb682ed279f37bf8356d2480f73e468b
Nova-472 <finnniansproject@gmail.com>
2020-05-19 19:43:52 +1200

    Update send.py

--- send.py
+++ send.py
@@ -1,5 +1,5 @@
 import paho.mqtt.publish as publish
 
-publish.single("CoreElectronics/test", "Hello", hostname="test.mosquitto.org")
-publish.single("CoreElectronics/topic", "World!", hostname="test.mosquitto.org")
+publish.single("", "Hello", hostname="test.mosquitto.org")
+publish.single("", "World!", hostname="test.mosquitto.org")
 print("Done")
